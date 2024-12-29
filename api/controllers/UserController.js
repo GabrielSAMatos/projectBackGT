229264c1db8c84a600df197cb49521dbb3faa768
Gabriel Matos <matos7771@hotmail.com>
2024-12-29 19:19:32 -0300

    Chamada de funcao para aplicar STATUS aplicada

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,4 +1,5 @@
 const UserModel = require('../models/UserModel');
+const check = require('../status/statusCheck.js');
 
 class UserController {
     async findAll(req, res){
@@ -11,7 +12,7 @@ class UserController {
             ]});
 
         return res.status(200).json(data);
-    }
+    };
 
     async findById(req, res){
 
@@ -24,13 +25,9 @@ class UserController {
             "email"
         ]});
 
-        if(product == null){
-            return res.status(404).json();
-        }
-        
-        return res.status(200).json(user);
+        return check.status200(res, user);
 
-    }
+    };
 
     async create(req, res){
         let body = req.body;
@@ -38,25 +35,29 @@ class UserController {
         res.status(201).json({
             message: "Usuario criado com sucesso!"
         });
-    }
+    };
 
     async upate(req, res){
         const id = req.params.id;
         const body = req.body;
+        let user = await UserModel.findByPk(id);
 
         await UserModel.update(body, { where: {id} });
 
-        return res.status(204).json();
+        return check.status204(res, user);
 
-    }
+
+    };
 
     async delete(req, res){
         const id = req.params.id;
+        let user = await UserModel.findByPk(id);
 
         await UserModel.destroy({ where: {id} });
 
-        return res.status(204).json();
-    }
+        return check.status204(res, user);
+
+    };
 
 }
 
