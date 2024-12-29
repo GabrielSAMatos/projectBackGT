fea2198ef6aac479538ba2f77da3ecb347cedf7e
Gabriel Matos <matos7771@hotmail.com>
2024-12-29 19:25:26 -0300

    Arquivo c/ funcoes  para retornarem status (200), (204) e (404)

---
+++ api/status/statusCheck.js
@@ -0,0 +1,16 @@
+module.exports = {
+    status200: (res, content) => {
+        if(content == null){
+            return res.status(404).json();
+        }
+        return res.status(200).json(content);
+    
+    },
+    status204: (res, content) => {
+        if(content == null){
+            return res.status(404).json();
+        }
+        return res.status(204).json(content);
+    
+    }
+};
