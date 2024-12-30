f3498fde08192b0c441e728a32e565a2684996d2
Gabriel Matos <matos7771@hotmail.com>
2024-12-30 03:50:02 -0300

    imgproductmodel att

--- api/database/syncAlter.js
+++ api/database/syncAlter.js
@@ -3,6 +3,8 @@ const connection = require('../config/connection');
 require('../models/CategoryModel');
 require('../models/ProductModel');
 require('../models/UserModel');
+require('../models/ImgProductModel');
+
 
 
 connection.sync({force:true});
