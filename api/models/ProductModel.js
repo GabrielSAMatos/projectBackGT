153f3456c8f257cec3a0d01407c9fd853c03e283
Gabriel Matos <matos7771@hotmail.com>
2024-12-30 16:32:37 -0300

    Associate adicionado ao ProductModel

--- api/models/ProductModel.js
+++ api/models/ProductModel.js
@@ -2,9 +2,14 @@ const { DataTypes, Model } = require('sequelize');
 const connection = require('../config/connection');
 
 class ProductModel extends Model {
-    static associate({ImgProductModel, OptionsModel}){
+    static associate({ImgProductModel, OptionsModel, CategoryModel, Product_CategoryModel}){
         ProductModel.hasMany(ImgProductModel, {foreignKey: "product_id"});
-        ProductModel.hasMany(OptionsModel, {foreignKey: "product_id"})
+        ProductModel.hasMany(OptionsModel, {foreignKey: "product_id"});
+        ProductModel.belongsToMany(CategoryModel, {
+            through: Product_CategoryModel,
+            foreignKey: 'product_id',
+            otherKey: 'category_id'
+        });
     }
 };
 
